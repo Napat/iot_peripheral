4284d439a229197720f83b98661fd9bb2abf8e32
Napat Rungruangbangchan <napat.joe@gmail.com>
2016-11-26 07:21:16 +0000

    mqtt publish and subscribe sample

--- mqtt_mosq/mqtt_pub.py
+++ mqtt_mosq/mqtt_pub.py
@@ -3,10 +3,12 @@ import time
 
 server_addr = "127.0.0.1"
 topic1 = "napat/channel1"
+topic2 = "napat/channel2"
 
 mqttc = mqtt.Client()
 mqttc.connect(server_addr, 1883)
 
 while True:
     mqttc.publish(topic1, "Halo")
+    mqttc.publish(topic2, "Haloo")
     time.sleep(1)
