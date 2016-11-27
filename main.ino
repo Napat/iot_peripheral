2c561cb7f4538c7f718c538af44c5d0ba7602849
Napat Rungruangbangchan <napat.joe@gmail.com>
2016-11-27 13:26:08 +0700

    nodemcuV2 input output sample

---
+++ main.ino
@@ -0,0 +1,10 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  setup_nodev2_iop();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  loop_nodev2_iop();
+}
---
+++ nodev2_io.ino
@@ -0,0 +1,27 @@
+
+#include "nodev2_io.h"
+
+void setup_nodev2_op(){
+  pinMode( LED_RED, OUTPUT);
+}
+
+void loop_nodev2_op(){
+  digitalWrite( LED_RED, LOW);
+  delay(500);
+  digitalWrite( LED_RED, HIGH);
+  delay(500);
+}
+
+void setup_nodev2_iop(){
+  pinMode( LED_RED, OUTPUT);
+  pinMode( BUTTON_01, INPUT);
+}
+
+void loop_nodev2_iop(){
+  if( digitalRead(BUTTON_01)){
+    digitalWrite( LED_RED, LOW);  
+  }else{
+    digitalWrite( LED_RED, HIGH);
+  }
+  delay(100);
+}
