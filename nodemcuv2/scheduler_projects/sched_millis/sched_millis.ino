9fa2cc4d8398bc62b72151b3ac670112da1cfc2d
Napat Rungruangbangchan <napat.joe@gmail.com>
2018-02-09 16:52:41 +0700

    nodemcu: scheduler using millis()

---
+++ nodemcuv2/scheduler_projects/sched_millis/sched_millis.ino
@@ -0,0 +1,39 @@
+
+#include "scheduler_millis.h"
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  scheduler_milis_atom();
+
+  if(scheduler_1msec)
+  {
+    static uint16_t cnt_1msec = 0;
+    cnt_1msec++;
+    if(cnt_1msec > 999)
+    {
+      Serial.print("[1msec x 1000]");
+      cnt_1msec = 0;
+    }
+  }
+
+  if(scheduler_10msec)
+  {
+    Serial.print(".");
+  }
+  
+  if(scheduler_100msec)
+  {
+    Serial.println("100 msec");   
+  }
+
+  if(scheduler_1sec)
+  {
+    Serial.println("1sec");   
+  }  
+}
+
---
+++ nodemcuv2/scheduler_projects/sched_millis/scheduler_millis.cpp
@@ -0,0 +1,46 @@
+#define __SCHEDULER_MILLIS_C__
+
+#include "scheduler_millis.h"
+
+void scheduler_milis_atom()
+{
+    static unsigned long mil_cur = millis();
+    static unsigned long mil_pre = mil_cur;
+    static uint16_t cnt_for_10msec = 0, cnt_for_100msec = 0, cnt_for_sec = 0;
+    mil_cur = millis();
+
+    scheduler_1msec = 0;
+    scheduler_10msec = 0;
+    scheduler_100msec = 0;
+    scheduler_1sec = 0;
+
+    // accounting for rollover(overflow) with subtraction!
+    if ((unsigned long)(mil_cur - mil_pre) >= INTERVAL_ATOM)
+    {
+        cnt_for_10msec++;
+        cnt_for_100msec++;
+        cnt_for_sec++;
+
+        scheduler_1msec = 1;
+
+        if (cnt_for_10msec > ((INTERVAL_10MSEC / INTERVAL_ATOM) - 1))
+        {
+            scheduler_10msec = 1;
+            cnt_for_10msec = 0;
+        }
+
+        if (cnt_for_100msec > ((INTERVAL_100MSEC / INTERVAL_ATOM) - 1))
+        {
+            scheduler_100msec = 1;
+            cnt_for_100msec = 0;
+        }
+
+        if (cnt_for_sec > ((INTERVAL_1SEC / INTERVAL_ATOM) - 1))
+        {
+            scheduler_1sec = 1;
+            cnt_for_sec = 0;
+        }
+
+        mil_pre = mil_cur;
+    }
+}
